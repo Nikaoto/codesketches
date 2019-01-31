74c8b0201b86b665e2d55298c8bf87d1af958c92
Nikaoto <nikaoto@gmail.com>
2019-01-31 21:38:57 +0400

    remove record func

--- 00/main.lua
+++ 00/main.lua
@@ -121,19 +121,10 @@ function love.draw()
                 pillar_height * 2,
                 pillar_width)
    end
-   
-   if shouldrecord then
-      lg.captureScreenshot(tostring(screens) .. ".png")
-      screens = screens + 1
-   end
 end
 
 function love.keypressed(k)
    if k == "escape" then
       love.event.quit()
-   end
-
-   if k == "r" then
-      shouldrecord = not shouldrecord
    end
 end
